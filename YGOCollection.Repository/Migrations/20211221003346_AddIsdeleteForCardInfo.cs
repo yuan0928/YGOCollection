c1b35b7f99182585ed5feebb059d9cc681a7031c
yuan0526 <freelion528@gmail.com>
2021-12-21 08:38:09 +0800

    CardInfo新增IsDelete欄位

---
+++ YGOCollection.Repository/Migrations/20211221003346_AddIsdeleteForCardInfo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YGOCollection.Repository.Migrations
+{
+    public partial class AddIsdeleteForCardInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "CardInfos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "CardInfos");
+        }
+    }
+}
